91d94914ba087562886e4b9f6d1e16f9e4cf5ec7
voldomarich <vbekasov1987@gmail.com>
2024-05-06 23:31:07 +0300

    Initial files upd

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int two = 2;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+    }
+}
---
+++ src/main/java/ru/job4j/calculator/MathFunction.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class MathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double subtraction(double first, double second) {
+        return second - first;
+    }
+
+    public static double division(double first, double second) {
+        return second / first;
+    }
+}
