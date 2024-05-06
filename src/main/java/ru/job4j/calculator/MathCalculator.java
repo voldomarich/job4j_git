b0243e0a10cef01cd20b7dac2e10e22c91342af7
voldomarich <vbekasov1987@gmail.com>
2024-05-06 23:56:42 +0300

    MathCalculator

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.calculator.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double subtractionAndDivision(double first, double second) {
+        return subtraction(first, second)
+                + division(first, second);
+    }
+
+    public static double allTogether(double first, double second) {
+        return sum(first, second)
+                + subtraction(first, second)
+                + multiply(first, second)
+                + division(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + subtractionAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + allTogether(10, 20));
+    }
+}
